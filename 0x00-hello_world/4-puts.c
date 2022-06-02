1f041a38f040a4db651d5cc7ac3fe577535d5d28
Rah-7 <rahigirmam16@gmail.com>
2022-06-02 13:51:13 -0700

    new task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
-
-
 /**
  *
- *  * main - Entry point
+ *  * main - first point
  *
- *   *
+ *   * Return: always 0
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *   */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }

bc40185944b01166b6f2a173c66510e63e785f8f
Rah-7 <rahigirmam16@gmail.com>
2022-06-03 12:00:15 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+int numR;
+int numl;
+int minl = 48;
+for (numR = 48; numR < = 57; numL++)
+{
+for (numR = minL++; numR < =57; numR++)
+{
+if (numR != numL)
+{
+putchar(numL);
+putchar(numR);
+if (!(numR == 57 && numL == 56))
+{
+putchar(',');
+puchhar('');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}

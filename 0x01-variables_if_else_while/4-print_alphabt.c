702130020a3f811861d6623bdd907d30e6e25f8b
Rah-7 <rahigirmam16@gmail.com>
2022-06-03 13:19:24 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.>
+/**
+ * main - Entry point
+ * Return: 0.
+ */
+int main(void)
+{
+char abc;
+for (abc = 'a'; abc < = 'z'; abc++)
+{
+if ((abc != 'e') && (abc != 'q'))
+putchar (abc);
+}
+putcha r('\n');
+return (0);
+}

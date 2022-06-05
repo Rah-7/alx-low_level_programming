4574a3813dc017efe8bc81f0447637fc83075182
Rah-7 <rahigirmam16@gmail.com>
2022-06-05 12:28:14 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,24 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
- * Return: 0.
+ * description: prints lowercase alphabets 
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-char abc;
-for (abc = 'a'; abc < = 'z'; abc++)
-{
-if ((abc != 'e') && (abc != 'q'))
-putchar r(abc);
-}
-putcha r('\n');
-return (0);
+	int ch;
+
+	for (ch = 'a'; ch < = 'z'; ch++
+	{
+		if (ch != 'e' && ch !='q'
+		{
+			putchar(ch);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
 }

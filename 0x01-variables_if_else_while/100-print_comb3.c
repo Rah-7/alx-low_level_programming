4d0c3a4abc9ab37ec5156b3a6bfe6d862d562a1e
Rah-7 <rahigirmam16@gmail.com>
2022-06-05 14:55:48 -0700

    Task 10 new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,33 +8,25 @@ int main(void)
 {
 
 	int numR;
-
 	int numL;
-
 	int minL = 48;
 
 	for (numL = 48; numL <= 57; numL++)
-
 	{
 
 		for (numR = minL++; numR <= 57; numR++)
-
 		{
-
 			if (numR != numL)
 
 			{
 
 				putchar(numL);
-
 				putchar(numR);
-
 				if (!(numR == 57 && numL == 56))
 
 				{
 
 					putchar(',');
-
 					putchar(' ');
 
 				}
@@ -46,7 +38,6 @@ int main(void)
 	}
 
 	putchar('\n');
-
 	return (0);
 
 }

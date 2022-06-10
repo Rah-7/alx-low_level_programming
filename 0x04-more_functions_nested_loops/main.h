64ada09e06f74531b6f9cc6c043a72d0d449daae
Rah-7 <rahigirmam16@gmail.com>
2022-06-10 00:43:36 -0700

    Task 6 new

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,10 +5,15 @@
  * void protoypes (void)
  * int protoypes (int)
  */
-int _isupper(int c);
-int _isdigit(int c);
+int _isupper(int);
+int _isdigit(int);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif /* MAIN_H */

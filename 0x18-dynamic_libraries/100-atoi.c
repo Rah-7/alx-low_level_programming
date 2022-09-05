33a5d4993158b3fa9fcadca41d7ced3e9edf37dd
Rah-7 <rahigirmam16@gmail.com>
2022-09-05 20:51:42 +0300

    new

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ *
+ * @s: the string
+ *
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	unsigned int len = 0, size = 0, p = 1, res = 0, sign = 1, i = 0;
+
+	while (*(s + len) != '\0')
+	{
+		if (size > 0 && (*(s + len) < '0' || *(s + len) > '9'))
+			break;
+
+		if (*(s + len) == '-')
+			sign *= -1;
+
+		if ((*(s + len) >= '0') && (*(s + len) <= '9'))
+		{
+			if (size > 0)
+				p *= 10;
+
+			size++;
+		}
+		len++;
+	}
+}
